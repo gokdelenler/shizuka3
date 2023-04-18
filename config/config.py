a6fa89068ac43ab9eaec0909287460781d66d557
gokdelenler <106874399+gokdelenler@users.noreply.github.com>
2023-04-18 18:23:02 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,11 +30,11 @@ UPSTREAM_REPO = getenv(
     "https://github.com/Papaa012/yenishizuki",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/gokdelenler/shizuka3")
+GITHUB_REPO = getenv("GITHUB_REPO", None)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iyiolmayolunda")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Best_FriendsFor_Ever")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", None)
 
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
